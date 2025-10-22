f9bda058b0cc2f53bea2a9b02534ac8617ca0c66
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-22 16:15:38 +0500

    stop

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-  // Import the functions you need from the SDKs you need
+  
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,4 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  console.log("good")
