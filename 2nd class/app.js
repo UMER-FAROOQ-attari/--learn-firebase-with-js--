1424273752b8c11980229e77d98f15e9b14e4cc7
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-10-29 16:43:53 +0500

    add & get

--- 2nd class/app.js
+++ 2nd class/app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getDatabase , set , ref} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+import { getDatabase , set , ref,get} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +22,9 @@ let email = document.getElementById("email")
 let rollNo = document.getElementById("rollNo")
 let btn = document.getElementById("btn")
 let password = document.getElementById("password")
+let getroll = document.getElementById("getroll")
+let gete = document.getElementById("gete")
+let answer = document.getElementById("answer")
 function add(name,email , rollNo, password){
  set(ref(db,"Users/"+ rollNo.value),{
     Name:name.value,
@@ -38,5 +41,23 @@ function add(name,email , rollNo, password){
 }
 btn.addEventListener("click",()=>{
     add(name,email,rollNo,password)
+})
+function getdata(){
+    const userdata = ref(db,"Users/"+getroll.value);
+    get(userdata).then((snapshot)=>{
+        if(snapshot.exists()){
+
+        let data = snapshot.val()
+        answer.innerHTML =`
+        Name : ${data.Name}<br>
+        Email:${data.Email}<br>
+        RollNumber:${data.RollNumber}<br>
+        Password:${data.Password}
+        `
+        }
+    })
+}
+gete.addEventListener("click",()=>{
+    getdata()
 })
 console.log("good")
