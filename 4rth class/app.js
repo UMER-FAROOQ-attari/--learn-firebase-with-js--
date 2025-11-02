2ede3da695004a8e7837ab98e59aec6744b0e042
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-02 13:00:29 +0500

    yes add

---
+++ 4rth class/app.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyASJMggxbUcQ15Qd5FJ0WaSzfX05agJbao",
+  authDomain: "learn-fairebase-fe48b.firebaseapp.com",
+  databaseURL: "https://learn-fairebase-fe48b-default-rtdb.firebaseio.com",
+  projectId: "learn-fairebase-fe48b",
+  storageBucket: "learn-fairebase-fe48b.firebasestorage.app",
+  messagingSenderId: "673280035319",
+  appId: "1:673280035319:web:a8033301dd1b83221286f2"
+};
+const app = initializeApp(firebaseConfig);
