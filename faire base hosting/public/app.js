9da1d188c9e3937b1c3d9b3414fe398ee16ce6a0
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-11-04 16:32:08 +0500

    full

--- faire base hosting/public/app.js
+++ faire base hosting/public/app.js
@@ -1,21 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getDatabase , set , ref,get} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, set, ref, get } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyASJMggxbUcQ15Qd5FJ0WaSzfX05agJbao",
-    authDomain: "learn-fairebase-fe48b.firebaseapp.com",
-    databaseURL: "https://learn-fairebase-fe48b-default-rtdb.firebaseio.com",
-    projectId: "learn-fairebase-fe48b",
-    storageBucket: "learn-fairebase-fe48b.firebasestorage.app",
-    messagingSenderId: "673280035319",
-    appId: "1:673280035319:web:a8033301dd1b83221286f2"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASJMggxbUcQ15Qd5FJ0WaSzfX05agJbao",
+  authDomain: "learn-fairebase-fe48b.firebaseapp.com",
+  databaseURL: "https://learn-fairebase-fe48b-default-rtdb.firebaseio.com",
+  projectId: "learn-fairebase-fe48b",
+  storageBucket: "learn-fairebase-fe48b.firebasestorage.app",
+  messagingSenderId: "673280035319",
+  appId: "1:673280035319:web:a8033301dd1b83221286f2"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 let name = document.getElementById("name")
 let email = document.getElementById("email")
@@ -25,39 +25,57 @@ let password = document.getElementById("password")
 let getroll = document.getElementById("getroll")
 let gete = document.getElementById("gete")
 let answer = document.getElementById("answer")
-function add(name,email , rollNo, password){
- set(ref(db,"Users/"+ rollNo.value),{
-    Name:name.value,
-    Email:email.value,
-    RollNumber:rollNo.value,
-    Password:password.value
- })
- .then(()=>{
-    console.log("data saved")
- })
- .catch((error)=>{
-  console.log("❌ Error: " + error)
- })
-}
-btn.addEventListener("click",()=>{
-    add(name,email,rollNo,password)
-})
-function getdata(){
-    const userdata = ref(db,"Users/"+getroll.value);
-    get(userdata).then((snapshot)=>{
-        if(snapshot.exists()){
-
-        let data = snapshot.val()
-        answer.innerHTML =`
-        Name : ${data.Name}<br>
-        Email:${data.Email}<br>
-        RollNumber:${data.RollNumber}<br>
-        Password:${data.Password}
-        `
-        }
+function add(name, email, rollNo, password) {
+  set(ref(db, "Users/" + rollNo.value), {
+    Name: name.value,
+    Email: email.value,
+    RollNumber: rollNo.value,
+    Password: password.value
+  })
+    .then(() => {
+      console.log("data saved")
+    })
+    .catch((error) => {
+      console.log("❌ Error: " + error)
     })
 }
-gete.addEventListener("click",()=>{
-    getdata()
+btn.addEventListener("click", () => {
+  add(name, email, rollNo, password)
+})
+function getdata() {
+  const userdata = ref(db, "Users/" + getroll.value);
+  get(userdata).then((snapshot) => {
+    if (snapshot.exists()) {
+
+      let data = snapshot.val()
+      answer.innerHTML = `
+<table>
+  <tr>
+    <th>Field</th>
+    <th>Value</th>
+  </tr>
+  <tr>
+    <td>Name</td>
+    <td>${data.Name}</td>
+  </tr>
+  <tr>
+    <td>Email</td>
+    <td>${data.Email}</td>
+  </tr>
+  <tr>
+    <td>Roll Number</td>
+    <td>${data.RollNumber}</td>
+  </tr>
+  <tr>
+    <td>Password</td>
+    <td>${data.Password}</td>
+  </tr>
+</table>
+`;
+    }
+  })
+}
+gete.addEventListener("click", () => {
+  getdata()
 })
 console.log("good")
